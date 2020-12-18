4adef9c9d0ab9733d4268df87eb66b0b5a713a94
Angry Monster <67206311+RKSouth@users.noreply.github.com>
2020-12-18 14:32:19 -0800

    added Dom elements

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+//DOM elements
+const resultEl = document.getElementById('result');
+const lengthEl = document.getElementById('length');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numbersEl = document.getElementById('numbers');
+const specialEl = document.getElementById('symbols');
+const generateEl = document.getElementById('generate');
+const clipboardEl = document.getElementById('clipboard');
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 var pass = "";
